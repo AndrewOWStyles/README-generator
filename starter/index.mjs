d61c524ac31e16f7d29feb906f68c64ddf290bbf
Andrew Styles <andystyles@hotmail.co.uk>
2023-02-19 12:58:16 +0000

    added all prompts for readme

--- starter/index.mjs
+++ starter/index.mjs
@@ -1,27 +1,28 @@
 import inquirer from 'inquirer';
 import fs from "fs/promises"
 
-let {description} = await inquirer
-  .prompt([
-    {
-        type: 'input',
-        name: 'title',
-        message: "Give a title for your project",
+let {
+  description
+} = await inquirer
+  .prompt([{
+      type: 'input',
+      name: 'title',
+      message: "Give a title for your project",
     },
     {
-        type: 'input',
-        name: 'description',
-        message: "Give a description for your project",
+      type: 'input',
+      name: 'description',
+      message: "Give a description for your project",
     },
     {
-        type: 'input',
-        name: 'installation',
-        message: "How can you install the project?",
+      type: 'input',
+      name: 'installation',
+      message: "How can you install the project?",
     },
     {
-        type: 'input',
-        name: 'usage',
-        message: "Provide instructions and examples for use",
+      type: 'input',
+      name: 'usage',
+      message: "Provide instructions and examples for use",
     },
     {
       type: 'list',
@@ -29,6 +30,27 @@ let {description} = await inquirer
       message: 'What license would you like to use?',
       choices: ['MIT', 'IBM', 'Mozilla', 'Unlicensed'],
     },
+    {
+      type: 'input',
+      name: 'contributions',
+      message: "list any collaborators, feel free to link their github profiles",
+    },
+    {
+      type: 'input',
+      name: 'tests',
+      message: "Describe how to use the application for someone seeing this README",
+    },
+    {
+      type: 'input',
+      name: 'github name',
+      message: "what is your github name? e.g. github.com/(github name)",
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: "what is your email address?",
+    },
+
   ])
 
 let READMEText = `
@@ -39,22 +61,19 @@ let READMEText = `
 `
 
 
-  fs.writeFile("README.md", READMEText)
+fs.writeFile("README.md", READMEText)
 
-  // when selecting license use a list to pick
-  // create a function and call it in the READMEText using ${generateLicense(license)}
+// when selecting license use a list to pick
+// create a function and call it in the READMEText using ${generateLicense(license)}
 
-  function generateLicense() {
-    if (license === "MIT") {
-      return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
-    }
-    else if (license === "IBM") {
-      return "[![License: IPL 1.0](https://img.shields.io/badge/License-IPL_1.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)"
-    }
-    else if (license === "Mozilla") {
-      return "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)"
-    }
-    else {
-      return "[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)"
-    }
+function generateLicense() {
+  if (license === "MIT") {
+    return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)"
+  } else if (license === "IBM") {
+    return "[![License: IPL 1.0](https://img.shields.io/badge/License-IPL_1.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)"
+  } else if (license === "Mozilla") {
+    return "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)"
+  } else {
+    return "[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)"
   }
+}
