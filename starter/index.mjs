54e67e4754de3adf2d0b6ed5448a5f50ae5432d2
Andrew Styles <andystyles@hotmail.co.uk>
2023-02-19 12:33:20 +0000

    added function for displaying license link based on prompt choice

--- starter/index.mjs
+++ starter/index.mjs
@@ -23,6 +23,12 @@ let {description} = await inquirer
         name: 'usage',
         message: "Provide instructions and examples for use",
     },
+    {
+      type: 'list',
+      name: 'license',
+      message: 'What license would you like to use?',
+      choices: ['MIT', 'IBM', 'Mozilla', 'Unlicensed'],
+    },
   ])
 
 let READMEText = `
@@ -34,13 +40,21 @@ let READMEText = `
 
 
   fs.writeFile("README.md", READMEText)
-  console.log(description);
 
   // when selecting license use a list to pick
   // create a function and call it in the READMEText using ${generateLicense(license)}
 
-//   function generateLicense() {
-//     if (license === "option") {
-//        return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
-//     }
-//   }
+  function generateLicense() {
+    if (license === "MIT") {
+      return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+    }
+    else if (license === "IBM") {
+      return "[![License: IPL 1.0](https://img.shields.io/badge/License-IPL_1.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)"
+    }
+    else if (license === "Mozilla") {
+      return "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)"
+    }
+    else {
+      return "[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)"
+    }
+  }
